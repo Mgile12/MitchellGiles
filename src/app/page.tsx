c51717077653de078f094617d8270a5bcdcf91a7
Mgile12 <mitchellgiles134@gmail.com>
2026-02-21 07:23:44 +1000

    Updated trace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero';
 import FAQSection from '@/components/FAQSection';
 import RulesSection from '@/components/RulesSection';
 import KillerResults from '@/components/KillerResults';
+import TrustedPlatforms from '@/components/TrustedPlatforms';
 import ServicesOverview from '@/components/ServicesOverview';
 import LocalSeoSection from '@/components/LocalSeoSection';
 import FinalCTA from '@/components/FinalCTA';
@@ -13,6 +14,7 @@ export default function HomePage() {
       <FAQSection />
       <RulesSection />
       <KillerResults />
+      <TrustedPlatforms />
       <ServicesOverview />
       <LocalSeoSection />
       <FinalCTA />
